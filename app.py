6c04674592b90bc767ae537666b924926aa6de91
Naomi Johnson <snjohnson789@gmail.com>
2025-02-04 12:01:50 -0800

    formatting

--- app.py
+++ app.py
@@ -2,21 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
 @app.route('/taxonomy')
 def taxonomy():
     return render_template('taxonomy.html')
 
+
 @app.route('/ratings')
 def ratings():
     return render_template('ratings.html')
+
 
 @app.route('/resources')
 def resources():
